2ed70dca34f5bc01a6c7e3cce35f02012dd591f0
Jon Kimble <jonathanbkimble@gmail.com>
2017-01-05 09:54:02 -0700

    Add test for visitors

---
+++ spec/features/visitor/visitor_cant_visit_user_pages_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'As a visitor' do
+  context 'I visit a user show path for a user' do
+    scenario 'It shows me a 404 error' do
+
+    end
+  end
+
+  context 'I visit a user edit path for a user' do
+    scenario 'It shows me a 404 error' do
+
+    end
+  end
+
+  context 'I visit a user experiment path for a user' do
+    scenario 'It shows me a 404 error' do
+
+    end
+  end
+end
