9f15654bb4c19a7c6282ab563445a1cd2992f146
Jon Kimble <jonathanbkimble@gmail.com>
2017-01-08 13:23:07 -0700

    Refactored signup route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :images
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'home#show'
-  get '/signup' => 'users#new'
+  get 'signup', to: 'users#new'
   resources :users, only: [:show, :edit, :create, :update, :destroy]
   # post '/users' => 'users#create'
   # get '/users/:id/edit' => 'users#edit'
