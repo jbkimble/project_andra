57753c49ba1f9ac77548a80e88f2c53ff54db758
Jon Kimble <jonathanbkimble@gmail.com>
2017-01-10 16:09:10 -0700

    Uncomment code

--- app/helpers/andra_emoji_helper.rb
+++ app/helpers/andra_emoji_helper.rb
@@ -1,19 +1,19 @@
-# module AndraEmojiHelper
-#   def self.find_expression(word)
-#       if word == "anger"
-#         "/assets/angry_andra.png"
-#       elsif word == "contempt"
-#         return "/assets/unhappy_andra.png"
-#       elsif word == "disgust"
-#         return "/assets/disgusted_andra.png"
-#       elsif word == "fear"
-#         return "/assets/scared_andra.png"
-#       elsif word == "happiness"
-#         return "/assets/really_happy_andra.png"
-#       elsif word == "sadness"
-#         return "/assets/sad_andra.png"
-#       else
-#         return "/assets/surprised_andra.png"
-#       end
-#   end
-# end
+module AndraEmojiHelper
+  def self.find_expression(word)
+      if word == "anger"
+        "/assets/angry_andra.png"
+      elsif word == "contempt"
+        return "/assets/unhappy_andra.png"
+      elsif word == "disgust"
+        return "/assets/disgusted_andra.png"
+      elsif word == "fear"
+        return "/assets/scared_andra.png"
+      elsif word == "happiness"
+        return "/assets/really_happy_andra.png"
+      elsif word == "sadness"
+        return "/assets/sad_andra.png"
+      else
+        return "/assets/surprised_andra.png"
+      end
+  end
+end
--- app/helpers/andra_language_helper.rb
+++ app/helpers/andra_language_helper.rb
@@ -1,19 +1,19 @@
-# module AndraLanguageHelper
-#   def self.andra_talk(word)
-#     if word == "anger"
-#       "angry!"
-#     elsif word == "contempt"
-#       "really unhappy..."
-#     elsif word == "disgust"
-#       return "disgusted."
-#     elsif word == "fear"
-#       "a little afraid."
-#     elsif word == "happiness"
-#       return "happy!"
-#     elsif word == "sadness"
-#       return "sad."
-#     else
-#       return "... surprised!"
-#     end
-#   end
-# end
+module AndraLanguageHelper
+  def self.andra_talk(word)
+    if word == "anger"
+      "angry!"
+    elsif word == "contempt"
+      "really unhappy..."
+    elsif word == "disgust"
+      return "disgusted."
+    elsif word == "fear"
+      "a little afraid."
+    elsif word == "happiness"
+      return "happy!"
+    elsif word == "sadness"
+      return "sad."
+    else
+      return "... surprised!"
+    end
+  end
+end
