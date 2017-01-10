baace882b6eace94db0a06ae2d62a52ccb3e5ea7
Jon Kimble <jonathanbkimble@gmail.com>
2017-01-10 15:09:26 -0700

    Comment out unused

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,16 +1,16 @@
-class PhotosController < ApplicationController
-  def new
-    @image = Image.new
-  end
-
-  def create
-    byebug
-    @image = Image.new(image_params)
-  end
-
-  private
-
-    def image_params
-      params.require(:image).permit(:id, :image)
-    end
-end
+# class PhotosController < ApplicationController
+#   def new
+#     @image = Image.new
+#   end
+#
+#   def create
+    # byebug
+#     @image = Image.new(image_params)
+#   end
+#
+#   private
+#
+#     def image_params
+#       params.require(:image).permit(:id, :image)
+#     end
+# end
