90ca1c72878adf543110f4b6fbd7f0c4a377492d
Jon Kimble <jonathanbkimble@gmail.com>
2017-01-10 15:10:14 -0700

    Remove unused method

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,10 +1,5 @@
 class RunsController < ApplicationController
 
-  # def create
-  #   byebug
-  #   @image = Image.new(image_params)
-  # end
-
   def new
     @experiment = Experiment.find(params[:experiment_id])
     @image = Image.new
