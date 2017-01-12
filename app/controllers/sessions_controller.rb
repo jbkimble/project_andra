4125abcf7d26564e6c3c7fb0047556824e68e240
Jon Kimble <jonathanbkimble@gmail.com>
2017-01-11 21:40:09 -0700

    Remove flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
-      flash[:success] =  "Welcome back #{@user.first_name}!"
+      # flash[:success] =  "Welcome back #{@user.first_name}!"
       session[:user_id] = @user.id
       redirect_to experiments_path
     else
